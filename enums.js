f9ce245db4918d0c849edac0e56f52f3ddc7d70c
Duchnh <hduy01012000@gmail.com>
2025-02-28 09:52:47 +0700

    FIX

--- enums.js
+++ enums.js
@@ -4,10 +4,10 @@ export const USER_ROLE = {
 };
 
 export const ROLE_NOTE = [
-  { key: 0, label: "CV" },
-  { key: 1, label: "APP" },
-  { key: 2, label: "DD" },
-  { key: 3, label: "AD" },
+  { key: "0", label: "CV" },
+  { key: "1", label: "APP" },
+  { key: "2", label: "DD" },
+  { key: "3", label: "AD" },
 ];
 
 export const CUSTOMER_STATUS = [
